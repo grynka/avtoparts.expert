e6d656d70fd7b4fe3bb6b1d8b9f3a7385028f7de
grynka <ihor.kozhemyakin@gmail.com>
2022-07-20 00:48:18 +0300

    item

---
+++ search.php
@@ -0,0 +1,10 @@
+<html>
+<div class="tagline"><svg class="tagline_icons"><use class="tagline_icon"
+                href="./images/icon.svg#car"></use></svg>еталі важливі
+            <p class="tagline_sub">сервіс із пошуку автозапчастин 
+в Україні</p>
+</div>
+                <form class="find">
+                    <input type="text" class="find_field" placeholder="Введіть код або назву запчастини">
+                </form>
+                </html>
